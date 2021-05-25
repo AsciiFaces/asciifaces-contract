7dac35897b154757ddc6f7bae35cfb7832468959
irfan marzuki <irfnmzk@gmail.com>
2021-05-25 22:30:17 +0700

    add basic task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,12 +5,12 @@ import "solidity-coverage";
 
 import "./tasks/accounts";
 import "./tasks/clean";
+import "./tasks/findFace";
 
 import { resolve } from "path";
 
 import { config as dotenvConfig } from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
-import { NetworkUserConfig } from "hardhat/types";
 
 dotenvConfig({ path: resolve(__dirname, "./.env") });
 
@@ -32,27 +32,6 @@ if (!process.env.MNEMONIC) {
   mnemonic = process.env.MNEMONIC;
 }
 
-let infuraApiKey: string;
-if (!process.env.INFURA_API_KEY) {
-  throw new Error("Please set your INFURA_API_KEY in a .env file");
-} else {
-  infuraApiKey = process.env.INFURA_API_KEY;
-}
-
-function createTestnetConfig(network: keyof typeof chainIds): NetworkUserConfig {
-  const url: string = "https://" + network + ".infura.io/v3/" + infuraApiKey;
-  return {
-    accounts: {
-      count: 10,
-      initialIndex: 0,
-      mnemonic,
-      path: "m/44'/60'/0'/0",
-    },
-    chainId: chainIds[network],
-    url,
-  };
-}
-
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   gasReporter: {
@@ -68,10 +47,6 @@ const config: HardhatUserConfig = {
       },
       chainId: chainIds.hardhat,
     },
-    goerli: createTestnetConfig("goerli"),
-    kovan: createTestnetConfig("kovan"),
-    rinkeby: createTestnetConfig("rinkeby"),
-    ropsten: createTestnetConfig("ropsten"),
   },
   paths: {
     artifacts: "./artifacts",
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,17 @@
-import { Contract } from "ethers";
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-// When running the script with `hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
-import { Greeter__factory } from "../typechain";
+import { AsciiFaces__factory } from "../typechain";
 
 async function main(): Promise<void> {
-  const Greeter: Greeter__factory = await ethers.getContractFactory("Greeter");
-  const greeter: Contract = await Greeter.deploy("Hello, Buidler!");
-  await greeter.deployed();
+  const WethMockFactory = await ethers.getContractFactory("WETHMock");
+  const AsciiFaceFactory: AsciiFaces__factory = await ethers.getContractFactory("AsciiFaces");
 
-  console.log("Greeter deployed to: ", greeter.address);
+  const wethMock = await WethMockFactory.deploy();
+  await wethMock.deployed();
+
+  const asciiface = await AsciiFaceFactory.deploy(wethMock.address);
+
+  console.log("Greeter deployed to: ", asciiface.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere and properly handle errors.
